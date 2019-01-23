c30f122821ac04f7f67b9aee845ab78193f17daa
azarrabi <azarrabi@umich.edu>
2019-01-23 16:13:20 -0500

    Added all the frontend code

---
+++ Starter App/frontend/app/src/main/java/com/tiberiuvilcu/chatter/Chatt.java
@@ -0,0 +1,17 @@
+package com.tiberiuvilcu.chatter;
+
+/**
+ * Created by tiberiuvilcu on 9/4/17.
+ */
+
+public class Chatt {
+    public String username;
+    public String message;
+    public String timestamp;
+
+    public Chatt(String username, String message, String timestamp) {
+        this.username = username;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
