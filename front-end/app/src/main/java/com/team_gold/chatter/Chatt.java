dd1b87c35e2328d994563d4c8e4c1847d1cc7abc
tobymac <tobymac@umich.edu>
2019-01-23 14:01:32 -0500

    Add front-end folder

---
+++ front-end/app/src/main/java/com/team_gold/chatter/Chatt.java
@@ -0,0 +1,12 @@
+package com.team_gold.chatter;
+
+public class Chatt {
+    public String username;
+    public String message;
+    public String timestamp;
+    public Chatt(String username, String message, String timestamp) {
+        this.username = username;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
