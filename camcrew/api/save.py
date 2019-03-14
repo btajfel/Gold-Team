e648f3187c703cee6259d81b92e5a5f99977694d
makaela <makaela@umich.edu>
2019-03-14 17:10:52 -0400

    working on file upload

--- camcrew/api/save.py
+++ camcrew/api/save.py
@@ -12,6 +12,22 @@ def post_save(projectid):
 	projectid = projectid
 	cur = crewcam.model.get_db().cursor()
 
+    # Create new filename hash.
+    # Save POST request's file object to a temp file
+    dummy, temp_filename = tempfile.mkstemp()
+    file = flask.request.files["file"]
+    file.save(temp_filename)
+
+    # Compute filename
+    new_filename = os.path.join(
+        camcrew.app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
+    # Move temp file to permanent location
+    shutil.move(temp_filename, new_filename)
+
+
 	# projects(projectid, name, owner, created)
 	if flask.request.method == 'POST':
 		cur.execute('INSERT INTO projects(postid, name, \
