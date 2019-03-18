e2fb458224f7ee42cd690d8f5c53be33aa5810f7
azarrabi <azarrabi@umich.edu>
2019-03-18 14:01:39 -0400

    changed up phonenumber column in db

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
   username VARCHAR(20) NOT NULL,
   fullname VARCHAR(40) NOT NULL,
   email VARCHAR(40) NOT NULL,
-  phonenumber INTEGER NOT NULL,
+  phonenumber VARCHAR(10) NOT NULL,
   password VARCHAR(256) NOT NULL,
   created TIMESTAMP NOT NULL,
   PRIMARY KEY(username)
