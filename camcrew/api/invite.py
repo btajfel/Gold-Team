557009409c180265dccc067363d6f61a60bcb184
azarrabi <azarrabi@umich.edu>
2019-03-26 15:22:24 -0400

    db change

--- camcrew/api/invite.py
+++ camcrew/api/invite.py
@@ -13,8 +13,6 @@ def invite():
     cur = camcrew.model.get_db().cursor()
 
     if flask.request.method == "POST":
-        print("data bytes", flask.request.data)
-        data_bytes = flask.request.data.decode('utf-8')
         print("data string", data_bytes)
         data = json.loads(flask.request.data)
         print("json")
@@ -24,7 +22,7 @@ def invite():
             cur.execute('\
                 INSERT INTO collaborators(projectid, username1, username2, created) \
                 VALUES (1, ?, ?, datetime("now", "localtime")) \
-                ', ("Brandon", user))
+                ', ("btajfel", user))
 
     contacts = cur.execute("""\
         SELECT fullname, username, phonenumber FROM users
