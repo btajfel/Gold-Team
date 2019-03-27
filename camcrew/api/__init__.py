1c4a3e0e4d058c9c5ad041fd584bd00da65ec542
azarrabi <azarrabi@umich.edu>
2019-03-26 21:34:37 -0400

    added login to init

--- camcrew/api/__init__.py
+++ camcrew/api/__init__.py
@@ -7,3 +7,4 @@ from camcrew.api.videos import get_videos
 from camcrew.api.invite import get_invite
 from camcrew.api.invite import post_invite
 from camcrew.api.location import get_location
+from camcrew.api.login import login
