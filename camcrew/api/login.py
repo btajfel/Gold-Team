216e22549b7e675af68a4c02e55e98951be21c80
Brandon Tajfel <btajfel@umich.edu>
2019-03-26 21:31:08 -0400

    created Login screen

--- camcrew/api/login.py
+++ camcrew/api/login.py
@@ -6,11 +6,14 @@ import camcrew
 from camcrew.api.error_handler import InvalidUsage
 
 
-@camcrew.app.route('/api/v1/login/<string:username>/<string:password>',
+@camcrew.app.route('/api/v1/login/',
                    methods=["POST"])
-def login(username, password):
+def login():
     context = {}
     cur = camcrew.model.get_db().cursor()
+    login_info = json.loads(flask.request.data)
+    username = login_info["username"]
+    password = login_info["password"]
 
     if flask.request.method == "POST":
         result = cursor.execute("""
@@ -20,7 +23,7 @@ def login(username, password):
             AND password = '%s'""" % (username, password)).fetchone()
         if not result:
             context['result'] = false
-            return flask.jsonify(**context), 403
+            return flask.jsonify(**context), 201
         else:
             context['result'] = true
     return flask.jsonify(**context), 201
