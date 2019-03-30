9eb570a884feeecbb08475c1c2b41833b0a03dcf
Trevor Sanderson <tjsande@umich.edu>
2019-03-30 14:31:22 -0400

    edit screen layout

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import { ScreenOrientation } from 'expo';
+import { AppRegistry, View, FlatList } from 'react-native';
+import { Row } from 'native-base';
 
 export default class EditScreen extends React.Component {
   static navigationOptions = {
@@ -20,6 +22,18 @@ export default class EditScreen extends React.Component {
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+        <FlatList/>
+      </View>
+
+    );
+
+
   }
 }
